ada48540a2554a120b85d5d8070979bcdfce8602
Ahasan Habib Sany <sany.baust@gmail.com>
2022-02-28 23:27:27 +0600

    mobile details feature done

--- scripts.js
+++ scripts.js
@@ -29,16 +29,15 @@ const fetchMobileData = async () => {
 
 document.getElementById('btn-search').addEventListener('click',fetchMobileData)
 
-
+// Handle Functionality for mobile Detail
 const mobileDetail = async (slug) =>{
     const url = `https://openapi.programming-hero.com/api/phone/${slug}`
 
     try{
         const res = await fetch(url)
         const data = await res.json()
-        console.log(data);
         if(data.status === true){
-            alert('sa')
+            showMobileDetail(data.data)
         }
 
     }catch(e){
@@ -49,8 +48,10 @@ const mobileDetail = async (slug) =>{
 // Function for show data 
 const showData = data =>{
     const resultContainer = document.getElementById('result')
-    data.forEach(mobile =>{
-        console.log(mobile);
+    resultContainer.innerHTML=''
+    data.forEach((mobile,i) =>{
+        console.log(i);
+        if(i<20){
         const resultdiv = document.createElement('div')
         resultdiv.classList.add('col-md-4')
         resultdiv.innerHTML =`  <div class="card mt-3">
@@ -67,8 +68,30 @@ const showData = data =>{
         </div>`;
     
     resultContainer.appendChild(resultdiv);
-    })
+    }
+})
     
 }
 
 // Function for handle phone data
+
+const showMobileDetail = (mobile) =>{
+    console.log(mobile);
+    const mobileDetailSection = document.getElementById('mobile-detail')
+    const divSection = document.createElement('div')
+    divSection.classList.add('card')
+    divSection.innerHTML =`  <div class="card mt-3">
+    <img src="${mobile.image}" class="card-img-top p-3 img-fluid  " 
+    style="width: 18rem; border-radius: 10px 10px 0 0"
+      alt="...">
+    <div class="card-body">
+    <p class="card-text">
+      <span class="badge bg-info text-white">${mobile.brand}</span>
+    </p>
+    <h5 class="card-title mb-3">${mobile.phone_name}</h5>
+    <button onclick="mobileDetail('${mobile.slug}')" type="button" class="w-100 btn btn-danger text-white">Explore</button>
+    </div>
+    </div>`
+
+    mobileDetailSection.appendChild(divSection)
+}
